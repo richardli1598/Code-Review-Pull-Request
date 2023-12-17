5f2eaf68ef251653165d672ca698468e758557b2
richardli1598 <opennaslab@gmail.com>
2023-12-17 10:05:07 +0800

    feat: only send patch to review
    Signed-off-by: richardli1598 <opennaslab@gmail.com>

--- src/github-pr-review.rs
+++ src/github-pr-review.rs
@@ -6,10 +6,6 @@ use github_flows::{
     octocrab::models::CommentId,
     EventPayload, GithubLogin
 };
-use http_req::{
-    request::{Method, Request},
-    uri::Uri,
-};
 use openai_flows::{
     chat::{ChatModel, ChatOptions},
     OpenAIFlows,
@@ -19,7 +15,7 @@ use std::env;
 //  The soft character limit of the input context size
 //   the max token size or word count for GPT4 is 8192
 //   the max token size or word count for GPT35Turbo is 4096
-static CHAR_SOFT_LIMIT : usize = 9000;
+// static CHAR_SOFT_LIMIT : usize = 9000;
 // static MODEL : ChatModel = ChatModel::GPT35Turbo;
 // static MODEL : ChatModel = ChatModel::GPT4;
 static MODEL : ChatModel = ChatModel::GPT4Turbo;
@@ -145,89 +141,31 @@ async fn handler(
     let pulls = octo.pulls(owner, repo);
     let mut resp = String::new();
     resp.push_str("Hello, I am a [code review bot](https://github.com/flows-network/github-pr-review/) on [flows.network](https://flows.network/). Here are my reviews of changed source code files in this PR.\n\n------\n\n");
-    match pulls.list_files(pull_number).await {
-        Ok(files) => {
-            for f in files.items {
-                let filename = &f.filename;
-                if filename.ends_with(".js") || filename.ends_with(".css") || filename.ends_with(".html") ||
-                    filename.ends_with(".htm") || filename.ends_with(".png") ||
-                    filename.ends_with(".jpg") || filename.ends_with(".gif") {
-                    continue;
+    match pulls.get_diff(pull_number).await {
+        Ok(diff) => {
+            resp.push_str("\n\n");
+
+            log::debug!("Sending patch to OpenAI:{}", pull_number);
+            let co = ChatOptions {
+                model: MODEL,
+                restart: true,
+                system_prompt: Some(system),
+                ..ChatOptions::default()
+            };
+            let question = "Review the following source code and look for potential problems. The code might be truncated. So, do NOT comment on the completeness of the source code.\n\n".to_string() + &diff;
+            match openai.chat_completion(&chat_id, &question, &co).await {
+                Ok(r) => {
+                    resp.push_str(&r.choice);
+                    resp.push_str("\n\n");
+                    log::debug!("Received OpenAI resp for patch:{}", pull_number);
                 }
-
-                // The f.raw_url is a redirect. So, we need to construct our own here.
-                let contents_url = f.contents_url.as_str();
-                if contents_url.len() < 40 { continue; }
-                let hash = &contents_url[(contents_url.len() - 40)..];
-                let raw_url = format!(
-                    "https://raw.githubusercontent.com/{owner}/{repo}/{}/{}", hash, filename
-                );
-                let file_uri = Uri::try_from(raw_url.as_str()).unwrap();
-                let mut writer = Vec::new();
-                match Request::new(&file_uri)
-                    .method(Method::GET)
-                    .header("Accept", "plain/text")
-                    .header("User-Agent", "Flows Network Connector")
-                    .send(&mut writer)
-                    .map_err(|_e| {}) {
-                        Err(_e) => {
-                            log::error!("Cannot get file");
-                            continue;
-                        }
-                        _ => {}
-                }
-                let file_as_text = String::from_utf8_lossy(&writer);
-                let t_file_as_text = truncate(&file_as_text, CHAR_SOFT_LIMIT);
-
-                resp.push_str("## [");
-                resp.push_str(filename);
-                resp.push_str("](");
-                resp.push_str(f.blob_url.as_str());
-                resp.push_str(")\n\n");
-
-                log::debug!("Sending file to OpenAI: {}", filename);
-                let co = ChatOptions {
-                    model: MODEL,
-                    restart: true,
-                    system_prompt: Some(system),
-                    ..ChatOptions::default()
-                };
-                let question = "Review the following source code and look for potential problems. The code might be truncated. So, do NOT comment on the completeness of the source code.\n\n".to_string() + t_file_as_text;
-                match openai.chat_completion(&chat_id, &question, &co).await {
-                    Ok(r) => {
-                        resp.push_str(&r.choice);
-                        resp.push_str("\n\n");
-                        log::debug!("Received OpenAI resp for file: {}", filename);
-                    }
-                    Err(e) => {
-                        log::error!("OpenAI returns error for file review for {}: {}", filename, e);
-                    }
-                }
-
-                log::debug!("Sending patch to OpenAI: {}", filename);
-                let co = ChatOptions {
-                    model: MODEL,
-                    restart: false,
-                    system_prompt: Some(system),
-                    ..ChatOptions::default()
-                };
-                let patch_as_text = f.patch.unwrap_or("".to_string());
-                let t_patch_as_text = truncate(&patch_as_text, CHAR_SOFT_LIMIT);
-                let question = "The following is a patch. Please summarize key changes.\n\n".to_string() + t_patch_as_text;
-                match openai.chat_completion(&chat_id, &question, &co).await {
-                    Ok(r) => {
-                        resp.push_str(&r.choice);
-                        resp.push_str("\n\n");
-                        log::debug!("Received OpenAI resp for patch: {}", filename);
-                    }
-                    Err(e) => {
-                        log::error!("OpenAI returns error for patch review for {}: {}", filename, e);
-                    }
+                Err(_e) => {
+                    log::error!("OpenAI returns error for patch review for {}", pull_number);
                 }
             }
         },
         Err(_error) => {
-            log::error!("Cannot get file list");
+            log::error!("Cannot get diff content");
         }
     }
 
@@ -238,12 +176,5 @@ async fn handler(
             log::error!("Error posting resp: {}", error);
         }
         _ => {}
-    }
-}
-
-fn truncate(s: &str, max_chars: usize) -> &str {
-    match s.char_indices().nth(max_chars) {
-        None => s,
-        Some((idx, _)) => &s[..idx],
     }
 }
